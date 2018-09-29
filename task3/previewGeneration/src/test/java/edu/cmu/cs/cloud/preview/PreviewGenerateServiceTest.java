8b14ebbb35e4e25fa3431437f3cc3c0e594121e1
Wen He <wenhe@andrew.cmu.edu>
2018-09-29 19:30:19 +0000

    task 3 preview generate finished

---
+++ task3/previewGeneration/src/test/java/edu/cmu/cs/cloud/preview/PreviewGenerateServiceTest.java
@@ -0,0 +1,15 @@
+package edu.cmu.cs.cloud.preview;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import org.junit.Test;
+
+public class PreviewGenerateServiceTest {
+
+    @Test
+    public void invokeTest() {
+        return;
+    }
+}
